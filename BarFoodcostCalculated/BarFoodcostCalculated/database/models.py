a98676b96d49968e109ec20acb4d49c2123347eb
Andrey Astahow <146195135+AndreyAstahow@users.noreply.github.com>
2025-08-28 17:00:35 +0300

    Add files via upload

---
+++ BarFoodcostCalculated/BarFoodcostCalculated/database/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class AlcoInfo(Base):
+    __tablename__ = 'Alcohol_info'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    volume = Column(Integer)
+    price = Column(Integer)
+
